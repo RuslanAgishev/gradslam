f91d165ee51d9bd02ab2624691547ef512efd994
RuslanAgishev <agishrus@fel.cvut.cz>
2021-09-20 10:31:32 +0200

    tested gradslam with sample bag file from subt sim

--- examples/pointfusion.py
+++ examples/pointfusion.py
@@ -35,6 +35,12 @@ parser.add_argument(
     " icp = Iterative Closest Point\n"
     " gradicp (*default) = Differentiable Iterative Closest Point\n",
 )
+parser.add_argument(
+    "--seqlen",
+    type=int,
+    default=10,
+    help="Sequence length (number of SLAM input frames)\n",
+)
 args = parser.parse_args()
 
 if __name__ == "__main__":
@@ -43,9 +49,9 @@ if __name__ == "__main__":
 
     # load dataset
     if args.dataset == "icl":
-        dataset = ICL(args.dataset_path, seqlen=10, height=120, width=160)
+        dataset = ICL(args.dataset_path, seqlen=args.seqlen, height=120, width=160)
     elif args.dataset == "tum":
-        dataset = TUM(args.dataset_path, seqlen=10, height=120, width=160)
+        dataset = TUM(args.dataset_path, seqlen=args.seqlen, height=120, width=160)
     loader = DataLoader(dataset=dataset, batch_size=2)
     colors, depths, intrinsics, poses, *_ = next(iter(loader))
 
@@ -53,7 +59,7 @@ if __name__ == "__main__":
     rgbdimages = RGBDImages(colors, depths, intrinsics, poses, channels_first=False)
 
     # SLAM
-    slam = PointFusion(odom=args.odometry, dsratio=4, device=device)
+    slam = PointFusion(odom=args.odometry, dsratio=1, device=device)
     pointclouds, recovered_poses = slam(rgbdimages)
 
     # visualization
